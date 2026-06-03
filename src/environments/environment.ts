374f4ac518bd4293eea04d6ac41e5f6befe0efe2
Andoni Gonzalo <andoni.gonzalo@siemens.com>
2026-06-03 21:53:31 +0200

    feat: enable blobs backend in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   adminPasswordHash: '0b27c0b522c51fe8f02bd973602180c75b14e140c0b48f9df44126dd45f978f0',
-  blobsBackend: false
+  blobsBackend: true
 };
 
 /*
